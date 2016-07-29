30721dfabe6a8ba12a2a347c15a215f326f6d83a
Suykoden <damasio321@hotmail.com>
2016-07-29 15:53:33 -0300

    Add project files.

---
+++ AngularBuscaCep/scripts/BuscaCepController.js
@@ -0,0 +1,38 @@
+﻿var app = angular.module('app', []);
+
+app.controller('BuscaCepController', function ($scope, $http) {
+     
+    $scope.Endereco = {
+        "cep": "",
+        "logradouro": "",
+        "complemento": "",
+        "bairro": "",
+        "localidade": "",
+        "uf": "",
+        "unidade": "",
+        "ibge": "",
+        "gia": ""
+    }
+
+    $scope.carregaCep = function (cep) {
+        $http
+        .get("https://viacep.com.br/ws/" + cep + "/json/")
+        .success(function (data) {
+            console.log(data);
+            $scope.dados = data;
+            console.log(data.cep);
+            $scope.Endereco.cep = data.cep;
+            $scope.Endereco.logradouro = data.logradouro;
+            $scope.Endereco.complemento = data.complemento;
+            $scope.Endereco.bairro = data.bairro;
+            $scope.Endereco.localidade = data.localidade;
+            $scope.Endereco.uf = data.uf;
+            $scope.Endereco.unidade = data.unidade;
+            $scope.Endereco.ibge = data.ibge;
+            $scope.Endereco.gia = data.gia;
+        })
+         .error(function () {
+             alert("Não foi possível carregar os dados");
+         });
+    }
+});
